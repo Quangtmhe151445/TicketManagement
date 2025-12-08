a08df1270e178511c9f0c23508713bed33dc814a
Phan Thao <phanthithao.4444@gmail.com>
2025-12-08 14:37:23 +0700

    add footer & navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import MyNav from "./navbar.js";
+import MovieList from "./movie_management/list-movie.js";
+import MyFooter from "./footer.js";
+
 function App() {
-    return (
-        <div>FER202 - Practical Exam given</div>
-    )
+  return (
+    <Router>
+      <MyNav />
+
+      <Routes>
+        <Route path="/movie-list" element={<MovieList />} />
+      </Routes>
+
+      <MyFooter/>
+    </Router>
+  );
 }
 
 export default App;
