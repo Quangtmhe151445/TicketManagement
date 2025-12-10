cacf896272c16110648aa84e161b43a19a8eb956
minh <minhdxhhe180142@fpt.edu.vn>
2025-12-10 14:10:17 +0700

    UPdate 1 chut

--- src/App.js
+++ src/App.js
@@ -6,22 +6,25 @@ import MyFooter from "./footer.js";
 import Ticket from "./TicketManager/TicketManager.js";
 import PopCornManager from "./PopCorn/PopCornManager.js";
 import CinemaRoomManager from "./CinemaRoom/CinemaRoomManager.js";
+import Login from "./timeTable/components/LoginForm.js";
 
 function App() {
   return (
     <Router>
       <MyNav />
       <div>
-        <Link to="/timetable">Go to TimeTable</Link>
-        <Link to="/cinema-rooms" className="ms-2 fw-bold text-success">Quản lý Phòng Chiếu</Link> |
+        <Link to="/cinema-rooms" className="ms-2 fw-bold text-success">
+          Quản lý Phòng Chiếu
+        </Link>
       </div>
-      
+
       {/* CHỈ CÓ MỘT KHỐI ROUTES DUY NHẤT */}
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<div>Chào mừng đến trang chủ!</div>} />
-        <Route path="/movie-list" element={<MovieList />}  />
-        <Route path="/ticket" element={<Ticket />}  />
-        <Route path="/popcorn" element={<PopCornManager />}  />
+        <Route path="/movie-list" element={<MovieList />} />
+        <Route path="/ticket" element={<Ticket />} />
+        <Route path="/popcorn" element={<PopCornManager />} />
         <Route path="/timetable" element={<Timetable />} />
         {/* Đường dẫn của component Quản lý Phòng Chiếu */}
         <Route path="/cinema-rooms" element={<CinemaRoomManager />} />
@@ -29,7 +32,6 @@ function App() {
 
       <MyFooter />
     </Router>
-
   );
 }
 
