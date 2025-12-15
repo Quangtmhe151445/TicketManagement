e5411643fa6465098f56fdb8aa2c4f351826ca3d
minh <minhdxhhe180142@fpt.edu.vn>
2025-12-15 20:09:07 +0700

    Fix db + Timetable

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import PopCornManager from "./PopCorn/PopCornManager.js";
 import CinemaRoomManager from "./CinemaRoom/CinemaRoomManager.js";
 import EditTicketPage from "./TicketManager/EditTicketPage.js";
 import MovieList from "./movie_management/list-movie.js";
-import Timetable from "./timeTable/components/Timetable";
+import Timetable from "./TimeTable/PersonnelManagement.js";
 import CreateTicketPage from "./TicketManager/CreateTicketPage.js";
 
 function App() {
